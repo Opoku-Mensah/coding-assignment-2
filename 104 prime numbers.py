501ceb9646fd7a056a3791aee507696b33709325
Opoku-Mensah <103118882+Opoku-Mensah@users.noreply.github.com>
2022-08-08 11:42:46 +0000

    Add files via upload

---
+++ 104 prime numbers.py
@@ -0,0 +1,12 @@
+int(input("Enter a number:"))
+n = int 
+sum=0
+for i in range(2,n+1):
+    for j in range(2,i):
+        if(i%j==0):
+            break
+    else: 
+        print(i,"is prime")
+        sum=sum+i
+        print("sum of prime numbers is",sum)
+        
